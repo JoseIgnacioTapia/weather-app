48927863c42b290cb036cfa6b842f742528a6cf7
José Ignacio <josignaciot@gmail.com>
2020-11-26 16:44:11 -0300

    Get Weather API Call

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,5 +1,19 @@
 const key = 'a88joq9O9CVIwSfxkYi0O3AYwSEg9GZO';
 
+// get weather information
+const getWeather = async (id) => {
+
+  const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+  const query = `${id}?apikey=${key}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+
+};
+
+// get city information
 const getCity = async (city) => {
 
   const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
@@ -12,6 +26,10 @@ const getCity = async (city) => {
 
 };
 
-getCity('manchester')
-  .then(data => console.log(data))
-  .catch(err => console.log(err));
+getCity('manchester').then(data => {
+  return getWeather(data.Key);
+}).then(data => {
+  console.log(data);
+}).catch(err => console.log(err));
+
+getWeather("329260");
